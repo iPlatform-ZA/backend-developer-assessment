2c9916b1eebfd8b7ba71268268bab43732620c94
Ronald <ronaldpasipanodya@users.noreply.github.com>
2015-11-02 20:17:01 +0200

    Releases EndPoint Update

--- IplatformBEAssessment/Models/DTOs/ReleaseDTO.cs
+++ IplatformBEAssessment/Models/DTOs/ReleaseDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IplatformBEAssessment.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,16 @@ namespace IplatformBEAssessment.Models.DTOs
 {
     public class ReleaseDTO
     {
+        public ReleaseDTO()
+        {
+            OtherArtists = new List<OtherArtists>();
+        }
+        public string ReleaseId { get; set; }
+        public string ReleaseTitle { get; set; }
+        public string Status { get; set; }
+        public string Label { get; set; }
+        public int NumberOfTracks { get; set; }
+        public ICollection<OtherArtists> OtherArtists { get; set; }
+
     }
 }
