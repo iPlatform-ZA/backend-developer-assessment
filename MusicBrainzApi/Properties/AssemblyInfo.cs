54992c0d8ebc9b49d2f7b05cef36dc1553171091
wernerva <werner.vanaswegen@gmail.com>
2016-08-11 22:53:52 +0200

    Cleaned up solution

--- MusicBrainzApi/Properties/AssemblyInfo.cs
+++ MusicBrainzApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MusicBrainzApi")]
+[assembly: AssemblyTitle("MusicBrainsAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MusicBrainzApi")]
+[assembly: AssemblyProduct("MusicBrainsAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f24c49a-8c1d-4029-8b22-f35ed28ce981")]
+[assembly: Guid("c435c86b-8c3c-48e2-a047-a98e1465ddeb")]
 
 // Version information for an assembly consists of the following four values:
 //
