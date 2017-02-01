e79dd3bc7551b9167322cb78a72d52f8ab5bf9f1
sox <sox@users.noreply.github.com>
2017-02-01 07:07:07 +0200

    Add files via upload

---
+++ DAL/ArtistRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ArtistRepository : IRepository
+    {
+        private MusicEntities _context;
+        public ArtistRepository(MusicEntities context)
+        {
+            _context = context;
+        }
+        public IQueryable<Artist> All
+        {
+            get
+            {
+                return _context.Artist;
+            }
+        }
+
+        public Artist GetOne(string id)
+        {
+            return _context.Artist.Where(x => x.ID.ToString().Equals(id)).FirstOrDefault();
+        }
+
+        public IQueryable<Artist> Search(string searchStr)
+        {
+            if(!string.IsNullOrEmpty(searchStr))
+            {
+                return  from artist in _context.Artist
+                            where artist.Name.StartsWith(searchStr) ||
+                            artist.Alias.Any(a => a.Name.StartsWith(searchStr))
+                            select artist;
+            }
+            return All;
+        }
+       
+    }
+}
---
+++ DAL/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using DAL.MusicBrainzJsonModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Album
+    {
+        public string releaseId { get; set; }
+        public string title { get; set; }
+        public string status { get; set; }
+        public string label { get; set; }
+        public int numberOfTracks { get; set; }
+        public List<OtherArtist> otherArtist { get; set; }
+    }
+}
